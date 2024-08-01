0f28cb5e0378349bd41910d747fca1dc9f5da3df
mp2720 <example@example.com>
2024-08-01 07:05:34 +0700

    added gsl

---
+++ src/types.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <gsl/pointers>
+#include <memory>
+
+namespace chat {
+
+using gsl::not_null, gsl::owner;
+using std::unique_ptr, std::weak_ptr, std::shared_ptr;
+
+} // namespace chat
